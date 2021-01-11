ae7e41fce7d7133eec4c4c5b5e109cff38ecbc96
takubokudori <takubokudori@gmail.com>
2021-01-11 22:13:17 +0900

    windy_macros

--- src/lib.rs
+++ src/lib.rs
@@ -34,6 +34,8 @@ use raw::*;
 #[cfg(not(feature = "no_std"))]
 pub use string::*;
 pub use windy_str::*;
+#[cfg(feature = "macros")]
+pub use windy_macros as macros;
 
 #[cfg(feature = "no_std")]
 #[allow(unused_imports)]
